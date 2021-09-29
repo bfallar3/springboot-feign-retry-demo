207bdff1decd7541e923ec34df0360f21bba95f1
Benjamin Fallar III <bfallar3@gmail.com>
2021-09-30 01:17:06 +0800

    updates

--- src/main/java/com/benjsoft/feignretryableexample/ClientController.java
+++ src/main/java/com/benjsoft/feignretryableexample/ClientController.java
@@ -1,12 +1,15 @@
 package com.benjsoft.feignretryableexample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("client")
 public class ClientController {
@@ -20,4 +23,22 @@ public class ClientController {
         return mockFeign.getObjects();
     }
 
+    @GetMapping("/divideBy/{num}")
+    public int divideWith(@PathVariable int num)
+    {
+        try {
+            return 100 / num;
+        }
+        catch (Exception e) {
+            // String err = ExceptionUtils.getStackTrace(e);
+            log.error("Arithmetic divide by zero encountered", e);
+        }
+        return 0;
+    }
+
+    @GetMapping("/greet/{country}/{name}")
+    public String sayName(@PathVariable String country, @PathVariable String name)
+    {
+        return "Hello " + name + " from " + country;
+    }
 }
